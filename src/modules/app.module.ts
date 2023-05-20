dbaa587040f5d56f3c132e8570455c98bbf7a91d
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2023-05-20 16:07:50 -0300

    fix: fixing imoports

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from '../config/guards/jwt-auth.guard';
 import { RolesGuard } from '../config/guards/roles.guard';
-import { LoggingMiddleware } from '../config/middleware/logging.middleware';
+import { LoggingMiddleware } from './../config/middleware/logging.middleware';
 import { AuthModule } from './auth.module';
 import { UserModule } from './user.module';
 
