c8d317715b22db05000475fbef725382f4a39758
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2023-05-27 19:01:50 -0300

    fix dependencs

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,20 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+
   app.enableCors();
 
   await app.listen(port, '0.0.0.0');
