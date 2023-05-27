4c979090126a991a98375b2baf871ae5ef858b1a
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2023-05-27 19:06:26 -0300

    fix dependencs

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: true,
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    },
-  });
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   const port = process.env.PORT || 3000;
