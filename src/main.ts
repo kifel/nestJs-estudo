8ed24734c86b09f4e3f47610f36f47546b80bb67
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2023-05-27 19:14:08 -0300

    fix dependencs

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
   const port = process.env.PORT || 3000;
 
   const config = new DocumentBuilder()
