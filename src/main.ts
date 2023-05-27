dca1eaf8d82b944867299d0c8d36d7fd6da8916d
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2023-05-27 18:45:25 -0300

    fix dependencs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cors from 'cors';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   const port = process.env.PORT || 3000;
 
   const config = new DocumentBuilder()
